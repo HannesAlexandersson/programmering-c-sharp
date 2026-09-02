814b87df099b47545c0c2ccf195f3b58147f8468
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:19:35 +0200

    Add ThresholdSum function to calculate sum above threshold
    Implement ThresholdSum function to calculate sum of values above a threshold.

---
+++ 07-arrays/solutions/assignment1/Program.cs
@@ -0,0 +1,20 @@
+﻿var input = new [] { 6, 1, 4, 5, 2, 8 };
+var output = ThresholdSum(input, 4);  // 6 + 5 + 8
+
+Console.WriteLine(output); // 19
+
+int ThresholdSum(int[] values, int threshold)
+{
+    var sum = 0;
+    foreach (var value in values)
+    {
+        if (value <= threshold)
+        {
+            continue;
+        }
+
+        sum += value;
+    }
+
+    return sum;
+}
