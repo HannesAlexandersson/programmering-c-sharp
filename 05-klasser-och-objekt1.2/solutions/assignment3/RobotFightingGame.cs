b67a11f4fb4eb09a7e3ca67674055cdd2f246d82
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:13:35 +0200

    Implement RobotFightingGame class for battle logic

---
+++ 05-klasser-och-objekt1.2/solutions/assignment3/RobotFightingGame.cs
@@ -0,0 +1,40 @@
+﻿namespace Assignment3
+{
+    public class RobotFightingGame
+    {
+        public Robot PlayerOne { get; }
+        public Robot PlayerTwo { get; }
+
+        public bool GameOver { get; set; }
+        public string? Winner { get; set; }
+
+        public RobotFightingGame(Robot playerOne, Robot playerTwo)
+        {
+            PlayerOne = playerOne;
+            PlayerTwo = playerTwo;
+        }
+
+        public void NextRound()
+        {
+            PlayerOne.Attack(PlayerTwo);
+            PlayerTwo.Attack(PlayerOne);
+
+            GameOver = PlayerOne.Health <= 0 || PlayerTwo.Health <= 0;
+
+            if (!GameOver)
+            {
+                return;
+            }
+
+            if (PlayerOne.Health > 0)
+            {
+                Winner = PlayerOne.Name;
+            }
+
+            if (PlayerTwo.Health > 0)
+            {
+                Winner = PlayerTwo.Name;
+            }
+        }
+    }
+}
