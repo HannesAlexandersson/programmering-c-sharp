59d0ef9c206fbb410e8d0df52d252ff891543602
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:11:31 +0200

    Add Program.cs with Cat class interactions

---
+++ 05-klasser-och-objekt1.2/solutions/assignment2/Program.cs
@@ -0,0 +1,10 @@
+﻿using Assignment2;
+
+var luna = new Cat("Luna", false);
+var artemis = new Cat("Artemis", true);
+
+Console.WriteLine($"{luna.Name} says: {luna.Speak()}");
+
+artemis.Eat(luna);  // Artemis is a zombie cat, turning Luna into a zombie cat too!
+
+Console.WriteLine($"{luna.Name} says: {luna.Speak()}");
