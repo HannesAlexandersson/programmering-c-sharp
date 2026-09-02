83f09dd4f281378cbe76b2db49a1fbfb90e1f63a
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 11:58:32 +0200

    Add Cinema class with movie display and validation

---
+++ 04-klasser-och-objekt1.1/solution4/Cinema.cs
@@ -0,0 +1,30 @@
+namespace Assignment4
+{
+    public class Cinema
+    {
+        public Movie Movie { get; set; }
+
+        public void MovieInformation()
+        {
+            Console.WriteLine($"We are currently showing: {Movie.Name} ({Movie.AgeRating} years or older, {Movie.Price}:-)");
+        }
+
+        public void ShowMovieTo(Person person)
+        {
+            if (person.Age < Movie.AgeRating)
+            {
+                Console.WriteLine($"I'm sorry {person.Name}, you are not old enough.");
+                return;
+            }
+
+            if(person.Money < Movie.Price)
+            {
+                Console.WriteLine($"I'm sorry {person.Name}, you don't have enough money.");
+                return;
+            }
+
+            person.Money -= Movie.Price;
+            Console.WriteLine($"Welcome {person.Name}, please enjoy the movie {Movie.Name}!");
+        }
+    }
+}
