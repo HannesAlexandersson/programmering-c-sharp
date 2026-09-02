9e19a5b6bb74c186910e41d4efb7d0d136b030ed
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:07:18 +0200

    Implement Game class with guess evaluation method

---
+++ 04-klasser-och-objekt1.1/solution8/Game.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment8
+{
+    public class Game
+    {
+        public int Correct { get; set; }
+
+        public int GuessNumber(int guess)
+        {
+            if (guess < Correct)
+            {
+                Console.WriteLine("too low");
+                return -1;
+            }
+
+            if (guess > Correct)
+            {
+                Console.WriteLine("too high");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
