1472255721233b9f109d2521f0713d2ac475ebce
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 11:57:53 +0200

    Add Person class with properties for Name, Age, and Money

---
+++ 04-klasser-och-objekt1.1/solution4/Person.cs
@@ -0,0 +1,9 @@
+namespace Assignment4
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Money { get; set; }
+    }
+}
