d2b10aadaf7fb018b2ad0d9911e7c84b44cec6e3
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:34:49 +0200

    Add shoreline counting functionality in Program.cs

---
+++ 09-LINQ/solutions/assignment2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Assignment2;
+
+var map = File.ReadAllLines("map.txt");
+var reader = new MapReader(map);
+var result = reader.CountShorelines();
+
+Console.WriteLine($"Meters of shoreline in map: {result * 10}");
