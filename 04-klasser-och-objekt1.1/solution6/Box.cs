95be26d3b5e7302be2c0ed80fdf3135f57474be7
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:00:25 +0200

    Add Box class with drawing functionality

---
+++ 04-klasser-och-objekt1.1/solution6/Box.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment6
+{
+    public class Box
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Draw(ConsoleColor color)
+        {
+            var originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            for (var row = 0; row < Height; row++)
+            {
+                for (var column = 0; column < Width; column++)
+                {
+                    Console.SetCursorPosition(X+column, Y+row);
+                    Console.Write("X");
+                }
+            }
+            Console.ForegroundColor = originalColor;
+        }
+    }
+}
