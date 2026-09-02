905ccf715bb02adbf92284a1b76788c87840bb34
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 11:25:11 +0200

    Add ticket selection feature to program
    Implement ticket selection and pricing logic.

---
+++ 01-förkunskaper/solution/program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Amazing Ticket Systems 1.0");
+Console.WriteLine();
+string answer;
+
+while (true)
+{
+    Console.WriteLine("Which ticket would you like?");
+    Console.Write("Type A, B or C:");
+    answer = Console.ReadLine().ToUpper();
+
+    if (answer == "A" || answer == "B" || answer == "C")
+    {
+        break;
+    }
+
+    Console.WriteLine("I'm sorry, that's not a valid ticket.");
+}
+
+var price = 0;
+
+if (answer == "A")
+{
+    price = 33;
+}
+
+if (answer == "B")
+{
+    price = 40;
+}
+
+if (answer == "C")
+{
+    price = 60;
+}
+
+Console.WriteLine($"You have selected {answer}, your total is ${price}");
