22b8cfa9500ae2f7a3b764346935cffa1164f9e3
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 11:54:18 +0200

    Delete 04-klasser-och-objekt1.1/solution directory

--- 04-klasser-och-objekt1.1/solution/program.cs
+++
@@ -1,8 +0,0 @@
-using Assignment1;
-
-var validator = new PasswordValidator();
-
-if (validator.IsValidPassword("p4ssw0rd"))
-{
-    Console.WriteLine("the password 'p4ssw0rd' is a valid password");
-}
