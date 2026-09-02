cd6a85e1c586ba1a91d99778bd64c7c304f61638
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:07:43 +0200

    Add Computer class with Update method for guessing game

---
+++ 04-klasser-och-objekt1.1/solution8/Computer.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment8
+{
+    public class Computer
+    {
+        public int Low = 1;
+        public int High = 100;
+
+        public void Update(int result, int guess)
+        {
+            if (result < 0 && guess >= Low)
+            {
+                Low = guess + 1;
+                return;
+            }
+
+            if (result > 0 && guess <= High)
+            {
+                High = guess - 1;
+                return;
+            }
+        }
+    }
+}
