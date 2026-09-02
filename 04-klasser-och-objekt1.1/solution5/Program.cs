06e51ccc492f2883ae0b2d3b8c688b2688a91f7c
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 11:59:04 +0200

    Add box creation and drawing functionality

--- 04-klasser-och-objekt1.1/solution5/Program.cs
+++ 04-klasser-och-objekt1.1/solution5/Program.cs
@@ -1,1 +1,25 @@
 
+﻿using Assignment5;
+
+// create two boxes
+var box1 = new Box()
+{
+    X = 3,
+    Y = 2,
+    Width = 7,
+    Height = 4
+};
+
+var box2 = new Box()
+{
+    X = 17,
+    Y = 4,
+    Width = 13,
+    Height = 8
+};
+
+// then draw both on the screen
+box1.Draw();
+box2.Draw();
+
+Console.ReadLine();
