214124eb92b577f0c4bb7539959abe4c9b7e66b7
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:20:36 +0200

    Add function to find duplicate numbers in an array
    Implement FindDuplicateNumber function to identify duplicates in an array.

---
+++ 07-arrays/solutions/assignment3/Program.cs
@@ -0,0 +1,23 @@
+﻿var number = FindDuplicateNumber([2, 14, 95, 8, 20, 14, 7, 3]);
+
+Console.WriteLine(number);  // 14
+
+int FindDuplicateNumber(int[] values)
+{
+    for (var i = 0; i < values.Length; i++)
+    {
+        var currentValue = values[i];
+        for (var j = i + 1; j < values.Length; j++)
+        {
+            var compareValue = values[j];
+            if (currentValue != compareValue)
+            {
+                continue;
+            }
+
+            return currentValue;
+        }
+    }
+
+    return -1;
+}
