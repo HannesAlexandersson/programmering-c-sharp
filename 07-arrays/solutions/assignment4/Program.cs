2838bf9edf7bde213eeab34be8b22f73791e6826
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:21:08 +0200

    Implement land tile calculation from map file

---
+++ 07-arrays/solutions/assignment4/Program.cs
@@ -0,0 +1,18 @@
+﻿var mapValues = File.ReadAllLines("map.txt");
+
+var landTiles = CalculateLandTilesInMap(mapValues);
+Console.WriteLine($"Number of tiles with land: {landTiles}");
+
+int CalculateLandTilesInMap(string[] map)
+{
+    var result = 0;
+    foreach (var row in map)
+    {
+        foreach (var tile in row)
+        {
+            result += tile == '#' ? 1 : 0;
+        }
+    }
+
+    return result;
+}
