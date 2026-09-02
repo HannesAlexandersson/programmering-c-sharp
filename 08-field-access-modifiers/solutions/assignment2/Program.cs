3e05c91782d7737f73e43ba30daeab5d4593a5a1
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:27:33 +0200

    Add change calculation logic in Program.cs
    Implement GetChangeForCustomer function to calculate change.

---
+++ 08-field-access-modifiers/solutions/assignment2/Program.cs
@@ -0,0 +1,24 @@
+﻿var change = GetChangeForCustomer(price: 100, pay: 100);
+// change = new [] { }
+change = GetChangeForCustomer(price: 32, pay: 50);
+// change = new [] { 10, 5, 2, 1 }
+change = GetChangeForCustomer(price: 501, pay: 1000);
+// change = new [] { 200, 200, 50, 20, 20, 5, 2, 2 }
+Console.WriteLine();
+int[] GetChangeForCustomer(int price, int pay)
+{
+    var register = new[] { 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+
+    var changeLeft = pay - price;
+    var change = new List<int>();
+    foreach (var cash in register)
+    {
+        while (changeLeft - cash >= 0)
+        {
+            change.Add(cash);
+            changeLeft -= cash;
+        }
+    }
+
+    return change.ToArray();
+}
