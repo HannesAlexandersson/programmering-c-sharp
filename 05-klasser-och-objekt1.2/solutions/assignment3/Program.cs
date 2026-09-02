d5bbd280d446d9f7150fe9419080d1c3a2d9c91c
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:12:40 +0200

    Implement Robot Fighting Game in Program.cs

---
+++ 05-klasser-och-objekt1.2/solutions/assignment3/Program.cs
@@ -0,0 +1,22 @@
+﻿using Assignment3;
+
+var megatron = new Robot("Megatron", 10);
+var optimus  = new Robot("Optimus Prime", 10);
+
+var game = new RobotFightingGame(megatron, optimus);
+
+// play the game until someone wins
+while (!game.GameOver)
+{
+    game.NextRound();
+    Thread.Sleep(400);
+}
+
+if (game.Winner == null)
+{
+    Console.WriteLine("The game ends in a draw! What a disappointment!");
+}
+else
+{
+    Console.WriteLine($"{game.Winner} is the winner!");
+}
