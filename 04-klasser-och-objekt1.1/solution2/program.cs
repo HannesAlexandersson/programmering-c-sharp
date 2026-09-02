d5596183fe787219112eed1c60c5d0c41afecd53
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 11:48:48 +0200

    Add program.cs to initialize Movie and Cinema

---
+++ 04-klasser-och-objekt1.1/solution2/program.cs
@@ -0,0 +1,15 @@
+﻿using Assignment2;
+
+var movie = new Movie()
+{
+    Name = "Ghostbusters",
+    AgeRating = 11,
+    Price = 60
+};
+
+var cinema = new Cinema()
+{
+    Movie = movie
+};
+
+cinema.MovieInformation();
