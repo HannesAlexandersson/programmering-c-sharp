e5ec7d6f5c6d5bedefb2a6ede44c9339a25237ec
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 11:56:25 +0200

    Implement movie and cinema classes with a person

---
+++ 04-klasser-och-objekt1.1/solution4/Program.cs
@@ -0,0 +1,26 @@
+using Assignment4;
+
+var movie = new Movie()
+{
+    Name = "Ghostbusters",
+    AgeRating = 11,
+    Price = 60
+};
+
+var cinema = new Cinema()
+{
+    Movie = movie
+};
+
+var person = new Person()
+{
+    Name = "Guybrush",
+    Age = 19,
+    Money = 100
+};
+
+Console.WriteLine($"{person.Name} has {person.Money}:- in his pocket");
+
+cinema.ShowMovieTo(person);
+
+Console.WriteLine($"{person.Name} has {person.Money}:- in his pocket");
