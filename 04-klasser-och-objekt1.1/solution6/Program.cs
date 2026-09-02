3ea8e30c76a039d9b35d7628814bf04f1cceb9ef
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:00:03 +0200

    Add Program.cs to create and draw Box objects
    Created a Program.cs file to instantiate and draw two Box objects with specified properties.

---
+++ 04-klasser-och-objekt1.1/solution6/Program.cs
@@ -0,0 +1,24 @@
+﻿using Assignment6;
+
+// create two boxes
+var box1 = new Box()
+{
+    X = 3,
+    Y = 2,
+    Width = 7,
+    Height = 4
+};
+
+var box2 = new Box()
+{
+    X = 17,
+    Y = 4,
+    Width = 13,
+    Height = 8
+};
+
+// then draw both on the screen
+box1.Draw(ConsoleColor.Red);
+box2.Draw(ConsoleColor.Blue);
+
+Console.ReadLine();
