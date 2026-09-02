52479cdd34ae66796730e5b9cedfe5b987951756
Hannes Alexandersson <S103154@gmail.com>
2026-09-02 12:11:01 +0200

    Implement FizzBuzz logic in Program.cs

---
+++ 05-klasser-och-objekt1.2/solutions/assignment1/Program.cs
@@ -0,0 +1,22 @@
+﻿for (var i = 1; i <= 100; i++)
+{
+    if (i % 15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+        continue;
+    }
+
+    if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+        continue;
+    }
+
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+        continue;
+    }
+
+    Console.WriteLine(i);
+}
